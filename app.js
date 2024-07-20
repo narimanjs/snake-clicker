4fa8e6658abb34867ba87f032d2f2ca9429abf0c
Nariman <mirmanov1@mail.ru>
2024-07-20 16:12:44 +0500

    create clicker

---
+++ app.js
@@ -0,0 +1,95 @@
+const $circle = document.querySelector("#circle");
+const $score = document.querySelector("#score");
+
+function start() {
+  setScore(getScore());
+  setImage();
+}
+
+function setScore(score) {
+  localStorage.setItem("score", score);
+  $score.textContent = score;
+}
+
+// function setImage() {
+//   if (getScore() >= 50) {
+//     $circle.setAttribute(
+//       "src",
+//       "./src/img/snakecoin2level-photoaidcom-cropped.png"
+//     );
+//   }
+//   if (getScore() >= 100) {
+//     $circle.setAttribute(
+//       "src",
+//       "./src/img/snakecoin3level-photoaidcom-cropped.png"
+//     );
+//   }
+//   if (getScore() >= 150) {
+//     $circle.setAttribute(
+//       "src",
+//       "./src/img/snakecoin4level-photoaidcom-cropped.png"
+//     );
+//   }
+// }
+
+function setImage() {
+  const score = getScore();
+  let src = "";
+
+  if (score >= 150) {
+    src = "./src/img/snakecoin4level-photoaidcom-cropped.png";
+  } else if (score >= 100) {
+    src = "./src/img/snakecoin3level-photoaidcom-cropped.png";
+  } else if (score >= 50) {
+    src = "./src/img/snakecoin2level-photoaidcom-cropped.png";
+  }
+
+  if (src) {
+    $circle.setAttribute("src", src);
+  }
+}
+
+function getScore() {
+  return Number(localStorage.getItem("score")) ?? 0;
+}
+
+function addOne() {
+  setScore(getScore() + 1);
+  setImage();
+}
+
+$circle.addEventListener("click", event => {
+  const rect = $circle.getBoundingClientRect();
+
+  const offfsetX = event.clientX - rect.left - rect.width / 2;
+  const offfsetY = event.clientY - rect.top - rect.height / 2;
+
+  const DEG = 40;
+
+  const tiltX = (offfsetY / rect.height) * DEG;
+  const tiltY = (offfsetX / rect.width) * -DEG;
+
+  $circle.style.setProperty("--tiltX", `${tiltX}deg`);
+  $circle.style.setProperty("--tiltY", `${tiltY}deg`);
+
+  setTimeout(() => {
+    $circle.style.setProperty("--tiltX", `0deg`);
+    $circle.style.setProperty("--tiltY", `0deg`);
+  }, 300);
+
+  const plusOne = document.createElement("div");
+  plusOne.classList.add("plus-one");
+  plusOne.textContent = "+1";
+  plusOne.style.left = `${event.clientX - rect.left}px`;
+  plusOne.style.top = `${event.clientY - rect.top}px`;
+
+  $circle.parentElement.appendChild(plusOne);
+
+  addOne();
+
+  setTimeout(() => {
+    plusOne.remove();
+  }, 2000);
+});
+
+start();
